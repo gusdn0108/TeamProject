795871f0d650760907eacf1babf53d5b0c25f4b1
gusdn0108 <gusdn6671@naver.com>
2022-02-23 09:25:44 +0900

    어드민 라우터분리

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require(`express`)
 const app = express()
 const router = express.Router()
 
-
+const adminRouter = require('./admin')
 const boardRouter = require(`./board`)
 const userRouter = require('./user/index.js')
 const mainController = require(`./main.Controller`)
@@ -13,6 +13,10 @@ app.use(express.urlencoded({extended:true}))
 
 router.use('/user', userRouter)
 router.use(`/board`,boardRouter)
+router.use('/admin',adminRouter)
 router.get(`/`,mainController.main)
+
+
+
 
 module.exports = router
